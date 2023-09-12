3d751f4de20b32058ba2efca7ff17043544509bf
Yaroslav Hos <yaroslav.hos@stryber.com>
2023-09-12 16:58:08 +0200

    refactoring done

---
+++ events-table/client/src/components/Event/styles.ts
@@ -0,0 +1,25 @@
+import {createUseStyles} from "react-jss";
+
+export default createUseStyles( {
+    eventContainer: {
+        //width: '100%',
+        minHeight: 100,
+        border: '1px solid grey',
+        borderRadius: 5,
+        padding: 10,
+        margin: 5,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#f2f2f2',
+        //justifyContent: 'space-evenly'
+    },
+    eventLine: {
+        width: '100%',
+    },
+    actionBtns: {
+        width: '100%',
+        textAlign: 'left',
+        padding: '5px 0',
+    },
+})
