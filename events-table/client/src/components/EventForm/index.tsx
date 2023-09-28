e93d09a5aed80172cf3008228de2f45356756564
Yaroslav Hos <yaroslav.hos@stryber.com>
2023-09-28 19:56:06 +0200

    Updated form component with MUI

--- events-table/client/src/components/EventForm/index.tsx
+++ events-table/client/src/components/EventForm/index.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Axios from 'axios';
+import {Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField} from "@mui/material";
 
 const EventForm = () => {
     const [eventName, setEventName] = useState('');
@@ -15,14 +16,42 @@ const EventForm = () => {
             alert('successful insert');
         })
     }
-
+    // const setEventName = (e: React.BaseSyntheticEvent) => {
+    //
+    // }
     return (
         <div className="form">
-            <label>Event name:</label>
-            <input type="text" name="eventName" onChange={(e) => setEventName(e.target.value)}/>
-            <label>Event severity:</label>
-            <input type="text" name="eventSeverity" onChange={(e) => setEventSeverity(e.target.value)}/>
-            <button onClick={submitEvent}>Submit</button>
+            <form onSubmit={submitEvent}>
+            <FormControl>
+                <FormLabel id="event-name">Event name</FormLabel>
+                <TextField
+                    type="text"
+                    aria-labelledby="event-name"
+                    label="Event name"
+                    variant="outlined"
+                    name="eventName"
+                    onChange={(e) => setEventName(e.target.value)}
+                />
+                <FormLabel id="severity">Severity</FormLabel>
+                <RadioGroup
+                    aria-labelledby="severity"
+                    defaultValue="High"
+                    name="eventSeverity"
+                >
+                    <FormControlLabel value="High" control={<Radio />} label="High" />
+                    <FormControlLabel value="Mid" control={<Radio />} label="Mid" />
+                    <FormControlLabel value="Low" control={<Radio />} label="Low" />
+                </RadioGroup>
+                <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="outlined">
+                    Submit
+                </Button>
+            </FormControl>
+            </form>
+            {/*<label>Event name:</label>*/}
+            {/*<input type="text" name="eventName" onChange={(e) => setEventName(e.target.value)}/>*/}
+            {/*<label>Event severity:</label>*/}
+            {/*<input type="text" name="eventSeverity" onChange={(e) => setEventSeverity(e.target.value)}/>*/}
+            {/*<button onClick={submitEvent}>Submit</button>*/}
         </div>
     )
 }
