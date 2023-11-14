ca8cac29e572158abedf85aef6d106f010832a97
Yaroslav Hos <yaroslav.hos@stryber.com>
2023-11-14 19:37:13 +0100

    Deleted old pages

--- events-table/client/src/components/Menu/EventsLayout.tsx
+++ events-table/client/src/components/Menu/EventsLayout.tsx
@@ -5,9 +5,6 @@ const EventsLayout = (props: any) => {
     const {data} = props
     return (<>
             <br/>
-            <Link to='/events/ignored'>Ignored</Link>
-            <br/>
-            <Link to='/events/reported'>Reported</Link>
             {/*<Link to='/events/:id'>One Event</Link>*/}
             <Outlet context={{data: data}}/>
         </>
